3b2269059a7fa6640112d4b45fa2697aec4f208c
Jan Baranowski <jan.baranowski@student.put.poznan.pl>
2014-06-18 18:04:01 +0200

    Added new version of client code plus some placeholders.

--- simple-client/src/main/java/com/example/cassandra/BoundStatementsClient.java
+++ simple-client/src/main/java/com/example/cassandra/BoundStatementsClient.java
@@ -5,25 +5,30 @@ import java.util.Set;
 import java.util.UUID;
 
 import com.datastax.driver.core.*;
+import com.datastax.driver.core.Cluster.Builder;
 
 public class BoundStatementsClient {
 	private Cluster cluster;
 	private Session session;
 	
-	public void connect(String node, int port) {
-	      cluster = Cluster.builder().withPort(port)
-	            .addContactPoint(node)
-	            .build();
-	      Metadata metadata = cluster.getMetadata();
-	      System.out.printf("Connected to cluster: %s\n", 
-	            metadata.getClusterName());
-	      for ( Host host : metadata.getAllHosts() ) {
-	         System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n",
-	               host.getDatacenter(), host.getAddress(), host.getRack());
-	      }
-	      session = cluster.connect();
-	   }
+	public static final int CASSANDRA_NATIVE_TRANSPORT_PORT = 9042;
 
+	public void connect(String node, int nattedNodesInsideCount) {
+		Builder b = Cluster.builder();
+		for (int i=0; i<nattedNodesInsideCount; i++) {
+		b.withPort(CASSANDRA_NATIVE_TRANSPORT_PORT+i).addContactPoint(node);
+		}
+		this.cluster = b.build();
+		
+		Metadata metadata = cluster.getMetadata();
+		
+		System.out.printf("Connected to cluster: %s\n", metadata.getClusterName());
+		for ( Host host : metadata.getAllHosts() ) {
+		System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n", host.getDatacenter(), host.getAddress(), host.getRack());
+		}
+		
+		this.session = cluster.connect();
+		}
 	   public void close() {
 	      cluster.close();
 	   }
@@ -67,6 +72,18 @@ public class BoundStatementsClient {
 		         "Bye Bye Blackbird'",
 		         "Joséphine Baker",
 		         tags ) );
+		   tags.clear();
+		   
+		   tags.add("rock");
+		   tags.add("1984");
+		   session.execute(boundStatement.bind(
+		         UUID.fromString("756716f5-2e54-4715-9f00-91dcbea6cf50"),
+		         "Bruce Springsteen'",
+		         "Born in the USA'",
+		         "Born in the USA",
+		         tags ) );
+		   tags.clear();
+		   
 		   
 		   statement = session.prepare(
 				      "INSERT INTO simplex.playlists " +
@@ -79,6 +96,12 @@ public class BoundStatementsClient {
 				      "La Petite Tonkinoise",
 				      "Bye Bye Blackbird",
 				      "Joséphine Baker") );
+				session.execute(boundStatement.bind(
+					      UUID.fromString("2cc9ccb7-6221-4ccb-8387-f22b6a1b354d"),
+					      UUID.fromString("756716f5-2e54-4715-9f00-91dcbea6cf50"),
+					         "Bruce Springsteen'",
+					         "Born in the USA'",
+					         "Born in the USA") );
 	   }
 	   
 	   public void querySchema(){
@@ -93,14 +116,19 @@ public class BoundStatementsClient {
 			System.out.println();
 	   }
 	   
+	   public void dropSchema(){
+		   session.execute(
+				      "DROP KEYSPACE IF EXISTS simplex;");
+	   }
+	   
 	   public static void main(String[] args) {
 		   BoundStatementsClient client = new BoundStatementsClient();
-		   client.connect("127.0.0.1", 9042);
+		   
+		   client.connect("localhost", 3);
+		   client.dropSchema();
 		   client.createSchema();
 		   client.loadData();
 		   client.querySchema();
-		   //client.updateSchema();
-		   //client.dropSchema();
 		   client.close();
 	   }
 	
--- simple-client/src/main/java/com/example/cassandra/SimpleClient.java
+++ simple-client/src/main/java/com/example/cassandra/SimpleClient.java
@@ -6,29 +6,38 @@ import com.datastax.driver.core.Metadata;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Cluster.Builder;
+import com.datastax.driver.core.ResultSet;
 
 public class SimpleClient {
 
+	public static final int CASSANDRA_NATIVE_TRANSPORT_PORT = 9042;
+	
 	private Cluster cluster;
 	private Session session;
 	
-	public void connect(String node, int port) {
-	      cluster = Cluster.builder().withPort(port)
-	            .addContactPoint(node)
-	            .build();
-	      Metadata metadata = cluster.getMetadata();
-	      System.out.printf("Connected to cluster: %s\n", 
-	            metadata.getClusterName());
-	      for ( Host host : metadata.getAllHosts() ) {
-	         System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n",
-	               host.getDatacenter(), host.getAddress(), host.getRack());
-	      }
-	      session = cluster.connect();
-	   }
-
-	   public void close() {
+	public void connect(String node, int nattedNodesInsideCount) {
+		Builder b = Cluster.builder();
+		for (int i=0; i<nattedNodesInsideCount; i++) {
+		b.withPort(CASSANDRA_NATIVE_TRANSPORT_PORT+i).addContactPoint(node);
+		}
+		this.cluster = b.build();
+		
+		Metadata metadata = cluster.getMetadata();
+		
+		System.out.printf("Connected to cluster: %s\n", metadata.getClusterName());
+		for ( Host host : metadata.getAllHosts() ) {
+		System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n", host.getDatacenter(), host.getAddress(), host.getRack());
+		}
+		
+		this.session = cluster.connect();
+		}
+	
+	 
+	
+	public void close() {
 	      cluster.close();
-	   }
+	 }
 
 	   public void createSchema() {
 		   session.execute("CREATE KEYSPACE simplex WITH replication " + 
@@ -86,15 +95,20 @@ public class SimpleClient {
 			System.out.println();
 	   }
 	   
+	   public void dropSchema(){
+		   session.execute(
+				      "DROP KEYSPACE IF EXISTS simplex;");
+	   }
+	   
+	   
 	   public static void main(String[] args) {
-	      SimpleClient client = new SimpleClient();	      	      
-	      client.connect("127.0.0.1", 9042);
-	      
-	      
-	      client.createSchema();
-	      client.loadData();
-	      client.querySchema();
-	      
-	      client.close();
+			SimpleClient client = new SimpleClient();
+			client.connect("localhost", 3);
+			client.dropSchema();
+			client.createSchema();
+			
+			client.loadData();
+			client.querySchema();
+			client.close();
 	   }
 }
